8107ac02db1489dd8cc900f8a885c577df50045a
abhinandha <abhinandha.puthiyottil @marketsimplified.com>
2022-02-14 16:52:08 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Login from './Login';
 import Home from './Home';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
           <div>
-            <Switch>
-              <Route path='/' exact><Login /></Route>
-              <Route path="/Home"><Home /></Route>
-            </Switch>
+            <Routes>
+              <Route path = '/home' element= {<Home />}></Route>
+              <Route path = '/' element= {<Login />}></Route>
+            </Routes>
           </div>
         </BrowserRouter>
       </header>
